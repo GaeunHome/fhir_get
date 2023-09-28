7c934de2a8605b0ab38c08ddc38105d4eafe68a2
侯文家 <gaeunhome27@gmail.com>
2023-09-28 13:40:30 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,25 @@
+import requests
+
+def read_fhir_appointments():
+    url = "http://localhost:8080/fhir/Appointment"  # 替換成FHIR Appointment數據URL
+    response = requests.get(url)
+    fhir_data = response.json()
+
+    # 獲得FHIR Appointment數據中的值
+    for entry in fhir_data['entry']:
+        appointment_id = entry['resource']['id']
+        appointment_description = entry['resource']['description']
+        appointment_start = entry['resource']['start']
+        appointment_end = entry['resource']['end']
+        participant_actor_reference = entry['resource']['participant'][0]['actor']['reference']
+
+        # 印出所有Appointment
+        print("Appointment ID:", appointment_id)
+        print("Appointment Description:", appointment_description)
+        print("Appointment Start:", appointment_start)
+        print("Appointment End:", appointment_end)
+        print("Participant Actor Reference:", participant_actor_reference)
+        print("-----------------------------------------------")
+
+# 執行讀取FHIR Appointment數據的函式
+read_fhir_appointments()
